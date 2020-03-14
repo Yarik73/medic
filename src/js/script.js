aa47baa6e4558761d8c6fa5f5aa196cd86613a84
Yarik73 <artamonovy6@gmail.com>
2020-03-14 13:50:35 +0400

    upd

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,14 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 var pageToggle = document.querySelector('.home-page');
+var navToggleArrow = document.querySelector('.main-nav__toggle-arrow');
 
 navToggle.addEventListener('click', function() {
+   navMain.classList.toggle('main-nav--opened');
+   pageToggle.classList.toggle('page-active');
+});
+
+navToggleArrow.addEventListener('click', function () {
    navMain.classList.toggle('main-nav--opened');
    pageToggle.classList.toggle('page-active');
 });
